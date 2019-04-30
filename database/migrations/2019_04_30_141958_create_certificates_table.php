2055dfa183b116564c637d77dd243652450562f7
Oyesola Ogundele <oyekunmi@gmail.com>
2019-04-30 15:28:40 +0100

    core: seed application tables

---
+++ database/migrations/2019_04_30_141958_create_certificates_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCertificatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('certificates', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('vessel_id');
+            $table->string('group');
+            $table->string('name');
+            $table->string('description');
+            $table->date('issue');
+            $table->date('expiry');
+            $table->string('renewals');
+            $table->string('next_renewal');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('certificates');
+    }
+}

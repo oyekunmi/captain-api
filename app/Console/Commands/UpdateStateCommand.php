f009a1de6593c0c79512dcfb5dbafbebb1627b07
Oyesola Ogundele <oyekunmi@gmail.com>
2019-05-02 11:30:56 +0100

    cron: create artisan command to run state updates in the background

--- app/Console/Commands/UpdateStateCommand.php
+++ app/Console/Commands/UpdateStateCommand.php
@@ -7,6 +7,9 @@ use Illuminate\Support\Facades\DB;
 use Carbon\Carbon;
 
 class UpdateStateCommand extends Command{
+
+    private $tableName = 'certificates';
+
    /**
      * The console command name.
      *
@@ -38,9 +41,8 @@ class UpdateStateCommand extends Command{
      */
     public function handle()
     {
-      $tableName = 'certificates';
 
-      DB::table($tableName)
+      DB::table($this->tableName)
         // ->whereIn('state', ['HEALTHY', 'EXPIRING'])
         ->chunkById(10, function($items){
           foreach($items as $item){
@@ -51,26 +53,30 @@ class UpdateStateCommand extends Command{
 
 
     private function process($item){
-      // DB::table($tableName)
-      //   ->where('id', $item->id)
-      //   ->update(['state'=>'EXPIRED']);
       
       $future = Carbon::today()->add('3', 'month');
       $expiry = Carbon::createFromFormat('Y-m-d', $item->expiry);
 
-      $monthsToExpiry = Carbon::now()->diffInMonths($expiry);
-      $this->info($monthsToExpiry);
+      $monthsToExpiry = Carbon::now()->diffInMonths($expiry, false);
 
       if($monthsToExpiry >= 0 && $monthsToExpiry < 3){
-        $this->info('EXPIRING');
+        $this->moveToState($item, 'EXPIRING');
       }
       else if( $monthsToExpiry > 2 ){
-        $this->info('HEALTHY');
+        $this->moveToState($item, 'HEALTHY');
       }
       else if ( $monthsToExpiry < 0 ){
-        $this->info('EXPIRED');
+        $this->moveToState($item, 'EXPIRED');
       }
 
+    }
+
+    private function moveToState($item, String $nextState){
+      if($item->state == $nextState) return ;
+
+      DB::table($this->tableName)
+        ->where('id', $item->id)
+        ->update(['state'=> $nextState]);
     }
 
 }

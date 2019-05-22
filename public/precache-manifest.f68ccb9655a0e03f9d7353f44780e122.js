7e11772f037ddf41765b784c3ce27ae036d4df5b
Oyesola Ogundele <oyekunmi@gmail.com>
2019-05-22 04:22:38 +0100

    release(v2.1.0): deployment for saving vessels on the web

---
+++ public/precache-manifest.f68ccb9655a0e03f9d7353f44780e122.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84fd3b9390fe04b4cb229792470a27dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "73b7b99956be6f6be7fe",
+    "url": "/static/js/5.577b3684.chunk.js"
+  },
+  {
+    "revision": "531d2ba3d5f44f326b22",
+    "url": "/static/js/6.19d0aa92.chunk.js"
+  },
+  {
+    "revision": "ca79a896bc1395b1ad61",
+    "url": "/static/js/Home.3e4aaa8f.chunk.js"
+  },
+  {
+    "revision": "0063bf32e9e1c33b34be",
+    "url": "/static/js/Login.06633242.chunk.js"
+  },
+  {
+    "revision": "a6f9da50ec6daef6be06",
+    "url": "/static/js/Settings.7f1e7fca.chunk.js"
+  },
+  {
+    "revision": "f4a358c5b383ea88275e",
+    "url": "/static/js/main.a5789d06.chunk.js"
+  },
+  {
+    "revision": "c67fb79b9c71efacf6f2",
+    "url": "/static/js/runtime~main.3aa0f231.js"
+  }
+]);
